0ceb0eda6f503e21c1a72dd34ae13c96581e7b4b
Alex Boiko <arekisanda@protonmail.com>
2020-07-11 23:24:08 +0300

    (optimize) Optimized error handling

--- api/server/middleware/errors.js
+++ api/server/middleware/errors.js
@@ -9,36 +9,41 @@
  */
 module.exports = (err, req, res, next) => {
     if (err) {
-        if (err.name === 'ValidationError') {
-            res.status(400).json({
-                status: 400,
-                error: 'Bad Request',
-                details: {
-                    name: err.name,
-                    details: err.message,
-                },
-            });
-        } else if (err.code === 11000) {
-            res.status(400).json({
-                status: 400,
-                error: 'User already exist',
-            });
-        } else if (err.name === 'JsonWebTokenError') {
-            res.status(401).json({
-                status: 401,
-                error: 'Unauthorized',
-            });
-        } else {
-            console.log(err.stack);
-            res.status(500).json({
-                status: 500,
-                error: 'Internal server error',
-                details: {
-                    name: err.name,
-                    message: err.message,
-                    code: err.code,
-                },
-            });
+        switch (err.name) {
+            case 'ValidationError':
+                res.status(400).json({
+                    status: 400,
+                    error: 'Bad Request',
+                    details: {
+                        name: err.name,
+                        details: err.message,
+                    },
+                });
+                break;
+            case 'JsonWebTokenError':
+                res.status(401).json({
+                    status: 401,
+                    error: 'Unauthorized',
+                });
+                break;
+            default:
+                if (err.code === 11000) {
+                    res.status(400).json({
+                        status: 400,
+                        error: 'User already exist',
+                    });
+                    break;
+                }
+                console.log(err.stack);
+                res.status(500).json({
+                    status: 500,
+                    error: 'Internal server error',
+                    details: {
+                        name: err.name,
+                        message: err.message,
+                        code: err.code,
+                    },
+                });
         }
     }
     return next();
